862175fe071ad69317296386f79315d8231d5bbd
Bogdan Petrovsky <fzzksiniy@icloud.com>
2022-01-16 20:54:41 +0300

    Added l7

---
+++ courses/l7/l7/p1/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace p1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Выбрать цвет из палитры";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(13, 54);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(98, 139);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Рисовать:";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(7, 109);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(81, 23);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Домик";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 79);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(82, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Окружности";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(7, 50);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(81, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Кружочки";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(7, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Клеточки";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Рисование на форме";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ courses/l7/l7/p1/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace p1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Color color;
+        Graphics graphics;
+        Pen pen; // линии
+        SolidBrush sb; // области
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            graphics = this.CreateGraphics();   
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                color = colorDialog1.Color;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!color.IsEmpty)
+            {
+                graphics.Clear(Color.White);
+                pen = new Pen(color);
+                for (int i = 0; i < this.ClientRectangle.Width; i += 10)
+                    graphics.DrawLine(pen, i, 0, i, this.ClientRectangle.Height);
+                for (int i = 0; i < this.ClientRectangle.Height; i += 10)
+                    graphics.DrawLine(pen, 0, i, this.ClientRectangle.Width, i);
+            }
+            else
+                MessageBox.Show("Выберите цвет");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            graphics.Clear(Color.White);
+            Random rand = new Random();
+            color = Color.FromArgb(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255));
+            sb = new SolidBrush(color);
+            pen = new Pen(color);
+            for (int i = 0; i < this.ClientRectangle.Width; i += 50)
+                for (int j = 0; j < this.ClientRectangle.Height; j += 50)
+                {
+                    graphics.FillEllipse(sb, i, j, 10, 10);
+                }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            graphics.Clear(Color.White);
+            Random rand = new Random();
+            for (int r = 0; r < this.ClientRectangle.Width; r += 10)
+            {
+                pen = new Pen(Color.FromArgb(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255)), 5);
+                graphics.DrawEllipse(pen, this.ClientRectangle.Width / 2 - r, this.ClientRectangle.Height / 2 - r, r * 2, r * 2);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            graphics.Clear(Color.White);
+            sb = new SolidBrush(Color.DarkRed);
+            Point[] points = new Point[4];
+            points[0].X = 200;
+            points[0].Y = 200;
+            points[1].X = 350;
+            points[1].Y = 150;
+            points[2].X = 500;
+            points[2].Y = 200;
+            points[3] = points[0];
+            graphics.FillPolygon(sb, points);
+            
+            sb = new SolidBrush(Color.Beige);
+            graphics.FillRectangle(sb, 210, 200, 280, 150);
+
+            pen = new Pen(Color.DarkGray, 5);
+            graphics.DrawLine(pen, 250, 230, 450, 230);
+            graphics.DrawLine(pen, 250, 300, 450, 300);
+            graphics.DrawLine(pen, 252, 230, 252, 300);
+            graphics.DrawLine(pen, 447, 230, 447, 300);
+
+            sb = new SolidBrush(Color.LightGray);
+            graphics.FillRectangle(sb, 255, 233, 190, 65);
+
+            graphics.DrawLine(pen, 250, 265, 450, 265);
+
+            pen = new Pen(Color.Brown, 3);
+            for (int i = 150; i < 550; i += 10)
+                graphics.DrawLine(pen, i, 320, i, 370);
+        }
+    }
+}
